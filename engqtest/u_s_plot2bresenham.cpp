7f3631c9ea71491e97853c83163808df66d34064
RickKoenig <mkoeni@sbcglobal.net>
2020-02-28 21:35:05 -0800

    line with float and int

--- engqtest/u_s_plot2bresenham.cpp
+++ engqtest/u_s_plot2bresenham.cpp
@@ -6,28 +6,47 @@
 
 // do bresenham line and circle algorithms
 using namespace u_plotter2;
+//#define FLOAT_LINE
+#define INT_LINE
+//#define FLOAT_CIRCLE
+//#define INT_CIRCLE
 
 namespace bresenham {
 
-	// implied line segment from 0 to 1
-		// x: over to the right some more
-		// y: move up and down
-	pointf2 p{ 3.0f, .5f };
-	float ang0, ang1, angd, sumdang, calch;
+	pointf2 pntItoF(const pointi2& pntI)
+	{
+		return pointf2x(float(pntI.x), float(pntI.y));
+	}
+#ifdef FLOAT_LINE
+	pointi2 endPoint{ 33,-12 }; // startPoint is at the origin
+	float slope; // from 0 to -1
+	float err;
 
 	// for debvars
 	struct menuvar bresenhamdv[] = {
-		{"@lightbrown@--- PLOTTER2 USER VARS for bresenham ---",NULL,D_VOID,0},
-		{"w",&p.x,D_FLOAT,FLOATUP / 16},
-		{"h",&p.y,D_FLOAT,FLOATUP / 16},
-		{"ang0",&ang0,D_FLOAT | D_RDONLY},
-		{"ang1",&ang1,D_FLOAT | D_RDONLY},
-		{"angd",&angd,D_FLOAT | D_RDONLY},
-		{"sumdang",&sumdang,D_FLOAT | D_RDONLY},
-		{"calch",&calch,D_FLOAT | D_RDONLY},
+		{"@cyan@--- PLOTTER2 USER VARS for bresenham ---",NULL,D_VOID,0},
+		{"slope",&slope,D_FLOAT | D_RDONLY},
+		{"endPointX",&endPoint.x,D_INT},
+		{"endPointY",&endPoint.y,D_INT}
 	};
 	const int nbresenhamdv = NUMELEMENTS(bresenhamdv);
+#endif
+#ifdef INT_LINE
+	pointi2 endPoint{ 33,-12 }; // startPoint is at the origin
+	S32 slopey;
+	S32 slopex;
+	S32 err;
 
+	// for debvars
+	struct menuvar bresenhamdv[] = {
+		{"@cyan@--- PLOTTER2 USER VARS for bresenham ---",NULL,D_VOID,0},
+		{"endPointX",&endPoint.x,D_INT},
+		{"endPointY",&endPoint.y,D_INT},
+		{"slopey",&slopey,D_INT | D_RDONLY},
+		{"slopex",&slopex,D_INT | D_RDONLY},
+	};
+	const int nbresenhamdv = NUMELEMENTS(bresenhamdv);
+#endif
 } // end namespace bresenham
 
 using namespace bresenham;
@@ -36,34 +55,62 @@ void plot2bresenhaminit()
 {
 	adddebvars("bresenham", bresenhamdv, nbresenhamdv);
 	plotter2init();
-	lzoom = -1.5f;
+	lzoom = -3.5f; // zoom way out
 }
 
 void plot2bresenhamproc()
 {
 	// interact with graph paper
 	plotter2proc();
-	// calc bresenham
-	ang0 = atan2(p.y, p.x);// *PIUNDER180;
-	ang1 = atan2(p.y, p.x - 1);// *PIUNDER180;
-	angd = ang1 - ang0;
-	//sumdang = (1 / p.x) * (1 / (1 + p.y*p.y / (p.x*p.x)));
-	//sumdang -= (1 / (p.x - 1)) * (1 / (1 + p.y*p.y / ((p.x-1)*(p.x-1))));
-	sumdang = p.x / (p.x*p.x + p.y*p.y) - (p.x - 1) / ((p.x - 1)*(p.x - 1) + p.y*p.y);
-	// formula to calc best angle for height
-	calch = sqrt(p.x*p.x - p.x);
 }
 
 void plot2bresenhamdraw2d()
 {
 	// draw graph paper
 	plotter2draw2d();
-	// draw bresenham
-	drawfline(pointf2x(0, 0), pointf2x(1, 0), C32BLACK);
-	drawfpoint(p, C32RED, 4.0f);
-	drawfpoint(pointf2x(p.x, calch), C32MAGENTA, 4.0f);
-	drawfline(pointf2x(0, 0), p, C32BLUE);
-	drawfline(pointf2x(1, 0), p, C32BLUE);
+	// calc and draw bresenham
+#ifdef FLOAT_LINE
+	endPoint.x = range(0, endPoint.x, 100);
+	endPoint.y = range(-endPoint.x, endPoint.y, 0);
+	if (endPoint.x != 0) // avoid a divide by zero
+		slope = float(endPoint.y) / endPoint.x;
+	else
+		slope = 0;
+	pointf2 pnt = pointf2x();
+	err = 0;
+	drawfpoint(pnt, C32RED, 4.0f);
+	for (auto i = 0; i < endPoint.x; ++i) {
+		++pnt.x;
+		err += slope;
+		// get a better error if we decrement y
+		if (err < -.5f) {
+			--pnt.y;
+			++err;
+		}
+		drawfpoint(pnt, C32RED, 4.0f);
+	}
+#endif
+#ifdef INT_LINE
+	endPoint.x = range(0, endPoint.x, 100);
+	endPoint.y = range(-endPoint.x, endPoint.y, 0);
+	slopex = endPoint.x;
+	slopey = endPoint.y;
+	err = 0;
+	pointi2 pnt = { 0,0 };
+	// err will be multiple of slopex*2
+	drawfpoint(pntItoF(pnt), C32RED, 4.0f);
+	for (auto i = 0; i < endPoint.x; ++i) {
+		++pnt.x;
+		err += slopey*2;
+		// get a better error if we decrement y
+		if (err < -slopex) {
+			--pnt.y;
+			err += 2*slopex;
+		}
+		drawfpoint(pntItoF(pnt), C32RED, 4.0f);
+	}
+
+#endif
 }
 
 void plot2bresenhamexit()
